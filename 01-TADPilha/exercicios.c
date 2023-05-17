ca15f88f2a80b8e99d76ec8e26a740c1adfd8dd4
Anderson <andersonlsilva03@gmail.com>
2023-05-17 09:32:55 -0300

    Pilha: Prova 20XX - questão 2

--- 01-TADPilha/exercicios.c
+++ 01-TADPilha/exercicios.c
@@ -21,6 +21,8 @@ int EX0Y(char *s, int n);
 // Prova 2022.1: Questão 3
 void RemoveMenoresQueChave(Stack *s, void *key, int *(cmp)(void *, void *));
 int *cmp(void *key, void *data);
+// Prova 20XX: questão 2
+int EX0Y0X(char *s, int n);
 
 int main()
 {
@@ -43,7 +45,7 @@ int main()
         printf("X=Y: FALSE");
     } */
 
-    Stack *s;
+    /*Stack *s;
     s = stkCreate(8);
     stkPush(s, (void *)10);
     stkPush(s, (void *)3);
@@ -54,7 +56,25 @@ int main()
     mostrarPilha(s);
     RemoveMenoresQueChave(s, (void *)4, cmp);
     printf("\n");
-    mostrarPilha(s);
+    mostrarPilha(s);*/
+
+    int n = 8, teste;
+    char *s;
+    char string_origem[] = "12012012";
+
+    s = malloc(strlen(string_origem) + 1);
+    strcpy(s, string_origem);
+    printf("String copiada: %s\n", s);
+
+    teste = EX0Y0X(s, n);
+    if (teste)
+    {
+        printf("string do tipo EX0Y0X: TRUE");
+    }
+    else
+    {
+        printf("string do tipo EX0Y0X: FALSE");
+    }
 
     return 0;
 }
@@ -154,4 +174,68 @@ int *cmp(void *key, void *data)
         return &resultado;
     }
     return &resultado;
+}
+
+/*Escreva um algoritmo que recebe um vetor de caracteres com somente
+os caracteres 1, 2 e um único caracter 0. e o tamanho do cetor que
+tem caracteres preenchidos (válidos). Este algoritmo deve verificar
+se a string que está armazenada é da forma x0y0x, onde x é o inverso
+de y. (se x="12221122", y ="22112221"). Todas as funções utilizadas
+no algoritmo devem estar descritas na prova.*/
+int EX0Y0X(char *s, int n)
+{
+    if (s != NULL && n > 0)
+    {
+        Stack *stk1, *stk2;
+        stk1 = stkCreate(n);
+        stk2 = stkCreate(n);
+        int i = 0, qtd = 0, aux = 0;
+        if (stk1 != NULL && stk2 != NULL)
+        {
+            while (s[i] != '0' && i < n)
+            {
+                stkPush(stk1, (void *)&s[i]);
+                i++;
+            }
+            qtd = i;
+            aux = qtd;
+            i++;
+            while (s[i] != '0' && aux > 0)
+            {
+                if (s[i] != *((char *)stkPop(stk1)))
+                {
+                    break;
+                }
+                stkPush(stk2, (void *)&s[i]);
+                i++;
+                aux--;
+            }
+            if (aux == 0)
+            {
+                i++;
+                aux = qtd;
+                while (s[i] != '0' && aux > 0)
+                {
+                    if (s[i] != *((char *)stkPop(stk2)))
+                    {
+                        break;
+                    }
+                    i++;
+                    aux--;
+                }
+                /*se a pilha acabar e ainda sobrar caracteres no segundo X,
+                então a string inicial é diferente da ultima
+
+                se a quantidade de valores na primeira string for maior do
+                que a quantidade de valores no segundo X, então a string
+                inicial é diferente da ultima*/
+                printf("%d", i);
+                if (i == n && aux == 0)
+                {
+                    return TRUE;
+                }
+            }
+        }
+    }
+    return FALSE;
 }
