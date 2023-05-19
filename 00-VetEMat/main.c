697f2872d07b336112ac665b82b4d987efc6d8ac
Anderson <andersonlsilva03@gmail.com>
2023-05-18 22:57:26 -0300

    Vetores/Matrizes: Prova 20XX(1) - Questão 2

--- 00-VetEMat/main.c
+++ 00-VetEMat/main.c
@@ -6,6 +6,8 @@
 
 // Prova 2022.1: Questão 1
 int MultiplicaMatrizPelaTRansposta(int *v1, int *v2, int *v3, int n);
+// Prova 20XX(1): Questão 2
+int MultiplicaV1porV2(int *v1, int *v2, int *v3, int n);
 
 int main()
 {
@@ -34,7 +36,8 @@ int main()
         printf("%d ", vet2[i]);
     }
 
-    MultiplicaMatrizPelaTRansposta(vet1, vet2, vet3, n);
+    // MultiplicaMatrizPelaTRansposta(vet1, vet2, vet3, n);
+    MultiplicaV1porV2(vet1, vet2, vet3, n);
 
     for (int i = 0; i < n * m; i++)
     {
@@ -42,6 +45,7 @@ int main()
     }
 }
 
+// Prova 2022.1: Questão 1
 /*Receber duas matrizes nxn armazenadas nos vetores v1 e v2 e , calcule
 uma nova matriz a ser armazenada no vetor v3 que corresponde a
 multiplicação da matriz v1 pela transposta de v2*/
@@ -60,6 +64,30 @@ int MultiplicaMatrizPelaTRansposta(int *v1, int *v2, int *v3, int n)
                 }
                 v3[i * n + j] = aux;
                 aux = 0;
+            }
+        }
+        return TRUE;
+    }
+    return FALSE;
+}
+
+// Prova 20XX(1): Questão 2
+/*Escreva um algoritmo que recebe duas matrizes A e B nxn
+armazenadas nos vetores v1 e v2, e retorna o vetor v3 com
+o resultado da multiplicação da matriz A pela matriz B*/
+int MultiplicaV1porV2(int *v1, int *v2, int *v3, int n)
+{
+    if (v1 != NULL && v2 != NULL && v3 != NULL)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                v3[i * n + j] = 0;
+                for (int k = 0; k < n; k++)
+                {
+                    v3[i * n + j] += v1[i * n + k] * v2[k * n + j];
+                }
             }
         }
         return TRUE;
