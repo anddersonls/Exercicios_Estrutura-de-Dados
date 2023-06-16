d9e552fabaa0f3789e6b408468a0bcf5b6994a7a
Anderson <andersonlsilva03@gmail.com>
2023-06-16 02:08:28 -0300

    Refatoração do algoritmo de n rainhas

--- 04-PilhaEncadeada/nRainhas.c
+++ 04-PilhaEncadeada/nRainhas.c
@@ -74,6 +74,43 @@ int main(void)
     printf("\nObrigado por utilizar meu codigo :) !");
 }
 
+void nRainhas(SLList *ls, int n)
+{
+    SLNode *cur;
+    int achou = TRUE;
+    cur = ls->first;
+    Posicao posicao;
+    Posicao *pos = &posicao;
+    pos->i = 0;
+    pos->j = 0;
+
+    while (pos->i < n)
+    {
+        achou = EPosPossivel(cur, pos, n);
+        if (achou)
+        {
+            adicionaRainha(ls, pos);
+            pos->j = 0;
+            pos->i++;
+        }
+        else if (achou == FALSE && pos->j >= n - 1)
+        {
+            retiraRainhaAnterior(ls, pos);
+            while (pos->j > n - 1)
+            {
+                retiraRainhaAnterior(ls, pos);
+            }
+        }
+        else
+        {
+            pos->j++;
+        }
+        achou = TRUE;
+        cur = ls->first;
+    }
+    desenhaTabuleiro(ls, n);
+}
+
 int EPosPossivel(SLNode *cur, Posicao *pos, int n)
 {
     int achou = TRUE, coluna, linha;
@@ -122,38 +159,6 @@ int EPosPossivel(SLNode *cur, Posicao *pos, int n)
     return achou;
 }
 
-void desenhaTabuleiro(SLList *ls, int n)
-{
-    Rainha *pos;
-    SLNode *cur;
-    cur = ls->first;
-    char matriz[n][n];
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            matriz[i][j] = 'X';
-        }
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        pos = (Rainha *)cur->data;
-        matriz[pos->lin][pos->col] = 'O';
-        cur = cur->next;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf(" %c ", matriz[i][j]);
-        }
-        printf("\n");
-    }
-}
-
 void adicionaRainha(SLList *ls, Posicao *pos)
 {
     Rainha *novaPos = (Rainha *)malloc(sizeof(Rainha));
@@ -170,39 +175,34 @@ void retiraRainhaAnterior(SLList *ls, Posicao *pos)
     pos->j = rainha->col + 1;
 }
 
-void nRainhas(SLList *ls, int n)
+void desenhaTabuleiro(SLList *ls, int n)
 {
+    Rainha *pos;
     SLNode *cur;
-    int achou = TRUE;
     cur = ls->first;
-    Posicao posicao;
-    Posicao *pos = &posicao;
-    pos->i = 0;
-    pos->j = 0;
+    char matriz[n][n];
 
-    while (pos->i < n)
+    for (int i = 0; i < n; i++)
     {
-        achou = EPosPossivel(cur, pos, n);
-        if (achou)
+        for (int j = 0; j < n; j++)
         {
-            adicionaRainha(ls, pos);
-            pos->j = 0;
-            pos->i++;
+            matriz[i][j] = 'O';
         }
-        else if (achou == FALSE && pos->j >= n - 1)
-        {
-            retiraRainhaAnterior(ls, pos);
-            while (pos->j > n - 1)
-            {
-                retiraRainhaAnterior(ls, pos);
-            }
-        }
-        else
-        {
-            pos->j++;
-        }
-        achou = TRUE;
-        cur = ls->first;
     }
-    desenhaTabuleiro(ls, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        pos = (Rainha *)cur->data;
+        matriz[pos->lin][pos->col] = 'R';
+        cur = cur->next;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf(" %c ", matriz[i][j]);
+        }
+        printf("\n");
+    }
 }
