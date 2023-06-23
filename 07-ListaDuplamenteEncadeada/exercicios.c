f5246dcec5d31f26cbf330d3f06e7f2ffc80394f
Anderson <andersonlsilva03@gmail.com>
2023-06-23 09:57:01 -0300

    ListaDupla: Questao 2 - Prova 2022.1

--- 07-ListaDuplamenteEncadeada/exercicios.c
+++ 07-ListaDuplamenteEncadeada/exercicios.c
@@ -21,6 +21,8 @@ int cmp(void *data, void *key);
 void mostrarlista(DLList *dll);
 // Questao 3: prova 20XX(6).pdf
 void *verificaNoCompartilhado(DLList *LL1, DLList *LL2);
+// Questao 2: Prova 2022.1
+void PegaElementosIguais(DLList *l1, DLList *l2, DLList *l3);
 
 int main(void)
 {
@@ -111,4 +113,48 @@ void *verificaNoCompartilhado(DLList *LL1, DLList *LL2)
         }
     }
     return NULL;
+}
+
+// Questao 2: Prova 2022.1
+/*Escreva um algoritmo que recebe três listas lineares duplamente encadeadas L1,
+L2 e L3. E, retorna a Lista L3 com os nós de L1 que estão que tem um igual em L2
+(data), ou seja que estão presentes em ambas as listas (L1 e L2). Não pode alocar novos
+nós. L3 é recebida pelo algoritmo sem nenhum nó (vazia). Os elementos incluídos em
+L3 devem ser retirados da lista L1.*/
+void PegaElementosIguais(DLList *l1, DLList *l2, DLList *l3)
+{
+    if (l1 != NULL && l1->first != NULL && l2 != NULL && l2->first != NULL && l3 != NULL)
+    {
+        DLNode *l1node, *l2node, *l3node;
+        l1node = l1->first;
+        while (l1node->next != NULL)
+        {
+            l2node = l2->first;
+            while (l2node->next != NULL)
+            {
+                if (l2node->data == l1node->data)
+                {
+                    if (l3->first == NULL)
+                    {
+                        l3->first = l1node;
+                        l3node = l3->first;
+                        l3node->next = NULL;
+                        break;
+                    }
+                    else
+                    {
+                        l3node->next = l1node;
+                        DLNode *l3prev;
+                        l3prev = l3node;
+                        l3node = l3node->next;
+                        l3node->prev = l3prev;
+                        l3node->next = NULL;
+                        break;
+                    }
+                                }
+                l2node = l2node->next;
+            }
+            l1node = l2node->next;
+        }
+    }
 }
