973e37f3440f528970086244ac23c2ae3c5d07a7
Anderson <andersonlsilva03@gmail.com>
2023-06-25 18:05:35 -0300

    Lista Simples: Questao 3 - Prova 2017.1

--- 06-ListaEncadeada/exercicios.c
+++ 06-ListaEncadeada/exercicios.c
@@ -19,6 +19,8 @@ int cmp(void *data, void *key);
 void mostraLista(SLList *sll);
 // Questao 1: Prova 2017.1
 SLList *sllCulmulativeSum(SLList *l1, int (*getvalue)(void *));
+// Questao 3: Prova 2017.1
+int InSub(SLList *l1, SLList *l2, int i1, int i2, int len);
 
 int main(void)
 {
@@ -32,6 +34,15 @@ int main(void)
     sllInsertLast(sll, (void *)9);
     sllInsertLast(sll, (void *)5);
 
+    sllInsertLast(sll2, (void *)3);
+    sllInsertLast(sll2, (void *)4);
+    sllInsertLast(sll2, (void *)0);
+    sllInsertLast(sll2, (void *)6);
+    sllInsertLast(sll2, (void *)1);
+
+    int teste;
+    teste = InSub(sll, sll2, 2, 1, 4);
+    printf("teste: %d", teste);
     mostraLista(sll);
 }
 
@@ -116,4 +127,53 @@ SLList *sllCulmulativeSum(SLList *l1, int (*getvalue)(void *))
         }
     }
     return NULL;
+}
+
+// Questao 3: Prova 2017.1
+/*Faça um algoritmo que recebe duas listas lineares simplesmente encadeadas
+l1 e l2 e três valores inteiros i1, i2 e len, e insere os nós da lista l2,
+começando pelo i2-esimo nó e continuando por len nós, na lista l1, começando
+antes do i1-ésimo  nó. Nenhum elemento da lista l1 deverá ser removido ou
+substituído; Se i1>length(l1)+1 (onde length indica o número de nós na lista)
+ou se i2+len-1>length(l2) ou se i1<1 ou se i2<l1 retorne FALSE. Os nós devem
+ser removidos de l2 e incluídos em l1. Não pode alocar novos nós.*/
+int InSub(SLList *l1, SLList *l2, int i1, int i2, int len)
+{
+    if (l1 != NULL && l2 != NULL && i1 > 0 && i2 > 0)
+    {
+        if (l1->first != NULL && l2->first != NULL)
+        {
+            int aux1 = 1, aux2 = 1;
+            SLNode *l1node, *l2node, *l1prev, *auxnode;
+            l1node = l1->first;
+            l2node = l2->first;
+            while (l1node->next != NULL && aux1 < i1)
+            {
+                l1prev = l1node;
+                l1node = l1node->next;
+                aux1++;
+            }
+            while (l2node->next != NULL && aux2 < i2)
+            {
+                l2node = l2node->next;
+                aux2++;
+            }
+            if (aux1 == i1 && aux2 == i2)
+            {
+                auxnode = l1node;
+                l1node = l1prev;
+                aux2 = 0;
+                while (l2node != NULL && aux2 < len)
+                {
+                    l1node->next = l2node;
+                    l2node = l2node->next;
+                    l1node = l1node->next;
+                    aux2++;
+                }
+                l1node->next = auxnode;
+                return TRUE;
+            }
+        }
+    }
+    return FALSE;
 }
